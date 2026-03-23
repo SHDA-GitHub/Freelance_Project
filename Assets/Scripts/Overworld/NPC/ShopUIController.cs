fdb08708069c9ca191fdabc07cf2f77f3bec2a60
SHDA-GitHub <180524544+SHDA-GitHub@users.noreply.github.com>
2026-03-23 10:32:54 +0100

    feat - added new music and sound effects

--- Assets/Scripts/Overworld/NPC/ShopUIController.cs
+++ Assets/Scripts/Overworld/NPC/ShopUIController.cs
@@ -142,7 +142,7 @@ public class ShopUIController : MonoBehaviour
     }
 
     void BuyItem(ShopItem item)
-    {
+    {        
         switch (item.type)
         {
             case ShopItemType.Item:
